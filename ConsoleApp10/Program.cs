89c161043c44936fe277a1a1994f9c39a34aa169
ivSalah <9loo7king@gmail.com>
2024-11-17 19:23:48 +0300

    this is q2

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -6,58 +6,12 @@ using System.Threading.Tasks;
 
 namespace Salah.H.W
 {
-    class student
-    {
-        public int id;
-        public string name;
-        public int mark;
-
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter the num of students: ");
-            Console.WriteLine("must be 5-10 :");
-            int c = int.Parse(Console.ReadLine());
-            if (c >= 5 && c <= 10)
-            {
-                List<student> list = new List<student>();
-                for (int i = 0; i < c; i++)
-                {
-                    student s = new student();
-                    Console.WriteLine("enter name of student: ");
-                    string name = Console.ReadLine();
-                    Console.WriteLine("enter ID of student: ");
-                    int id = int.Parse(Console.ReadLine());
-                    Console.WriteLine("enter mark of student :");
-                    int mark = int.Parse(Console.ReadLine());
-                    s.mark = mark;
-                    s.name = name;
-                    s.id = id;
-                    list.Add(s);
 
-                }
-                bool found = false;
-                Console.WriteLine("student how pass is : ");
-                for (int i = 0; i < list.Count; i++)
-                {
-                    if (list[i].mark >= 50)
-                    {
-                        found = true;
-                        Console.WriteLine("the name student :" + list[i].name);
-                        Console.WriteLine("the id of student :" + list[i].id);
-                    }
-                }
-                if (!found)
-                    Console.WriteLine("no one  pass ");
-                
-
-            }
-            else 
-                Console.WriteLine("the student must be 5-10. ");
-                Console.ReadLine();
-            
+            Console.WriteLine("mohammed");
         }
     }
 }
